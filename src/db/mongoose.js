b57b7fc778373db56e7ff75cc7c758ffefb0c8bd
Deepanshu Malviya <deepanshumalviya8964@gmail.com>
2020-08-28 01:30:46 +0530

    get data

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/eGrocery', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
--- src/server.js
+++ src/server.js
@@ -1,18 +1,29 @@
 const express = require('express')
+const User = require('./users');
 require('./db/mongoose')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.get("/users", (req, res, next) => {
-    users.find().then(documents => {
+    console.log("s");
+    User.find().then(documents => {
       res.status(200).json({
         message: "Posts fetched successfully!",
         lastIdUsed: documents
       });
+      console.log(documents);
     });
   });
+//  one();
+//   function one(){
+//     User.find().then(documents => {
+        
+//         console.log(documents);
+//       });
+//   }
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
