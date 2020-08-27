4a097ef9e36f9cc26c595c20e916894dbd9e7432
Deepanshu Malviya <deepanshumalviya8964@gmail.com>
2020-08-28 01:39:35 +0530

    get data

--- src/server.js
+++ src/server.js
@@ -7,22 +7,18 @@ const port = process.env.PORT
 
 app.use(express.json())
 app.get("/users", (req, res, next) => {
-    console.log("s");
-    User.find().then(documents => {
       res.status(200).json({
         message: "Posts fetched successfully!",
-        lastIdUsed: documents
       });
-      console.log(documents);
-    });
+      
   });
-//  one();
-//   function one(){
-//     User.find().then(documents => {
+ one();
+  function one(){
+    User.find().then(documents => {
         
-//         console.log(documents);
-//       });
-//   }
+        console.log(documents);
+      });
+  }
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
